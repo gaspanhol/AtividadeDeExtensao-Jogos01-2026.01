f322df14c5894f91ea73415169ffe0ac6f785783
lololibc <160233179+lololibc@users.noreply.github.com>
2026-06-12 20:58:57 +0000

    fix: Tirar debug, tela de vitória e correção no mapa

--- src/scenes/vitoria.js
+++ src/scenes/vitoria.js
@@ -14,20 +14,6 @@ export default class Vitoria extends Phaser.Scene {
             .setOrigin(0, 0)
             .setDisplaySize(this.scale.width, this.scale.height);
 
-        // texto de instrução
-        this.add.text(
-            this.scale.width / 2,
-            this.scale.height / 2 + 150,
-            "Parabéns! Você ganhou! \n Clique em qualquer lugar para jogar novamente",
-            {
-                fontSize: "20px",
-                color: "#ffffff",
-                align: "center",
-                backgroundColor: "rgba(0,0,0,0.5)",
-                padding: { x: 10, y: 5 }
-            }
-        ).setOrigin(0.5);
-
         // clique em qualquer lugar da tela
         this.input.once("pointerdown", () => {
             // recarrega a página inteira (zera tudo)
